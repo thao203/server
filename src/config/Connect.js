8ad05d1e8b1ec096899772fca1622a83cc9cc9e0
thao203 <nttthao.dhti15a4hn@sv.uneti.edu.vn>
2025-05-04 22:00:21 +0700

    first commit

--- src/config/Connect.js
+++ src/config/Connect.js
@@ -1,17 +1,19 @@
-﻿const Mongoose = require('mongoose');
+﻿const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await Mongoose.connect(process.env.CONNECT_DB, {
+        console.log("MongoDB URI:", process.env.CONNECT_DB); // Log URI để debug
+
+        await mongoose.connect(process.env.CONNECT_DB, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
+
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Failed to connect to MongoDB', error);
     }
-
 };
 
 module.exports = connectDB;
