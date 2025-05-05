71dbc3478a953af5cf156ba402b554a2dd714978
thao203 <nttthao.dhti15a4hn@sv.uneti.edu.vn>
2025-05-06 04:49:27 +0700

    Update EmailRouter.js

--- src/route/EmailRouter.js
+++ src/route/EmailRouter.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const ControllerEmail = require('../Controller/ControllerEmail');
 
 router.post('/api/sendOTP', ControllerEmail.sendOTP); 
+router.post('/api/notifyOverdue', ControllerEmail.notifyOverdue);
+router.post('/api/notifyDueSoon', ControllerEmail.notifyDueSoon);
 module.exports = router;
